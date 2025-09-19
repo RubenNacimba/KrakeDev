92bf3aace1a6d87e8970e925675cfa7ac6fe5f74
RubenNacimba <mundoinformaticoz.com@gmail.com>
2025-09-19 00:08:45 -0500

    El boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar = function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imagenSalud");
+    cmpImagenSaludo.src="./imagenes/ganar2.jpeg";
     console.log("Ingresa a saludar");
 }
