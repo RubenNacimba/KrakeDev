0c3c038611a1895adf2a276baa3d6c3cc17b4e12
RubenNacimba <mundoinformaticoz.com@gmail.com>
2025-09-20 12:08:37 -0500

    Se agrega un dif a la pagina

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,6 +1,6 @@
 saludar = function(){
     let cmpImagenSaludo;
     cmpImagenSaludo=document.getElementById("imagenSalud");
-    cmpImagenSaludo.src="./imagenes/ganar2.jpeg";
+    cmpImagenSaludo.src="./imagenes/imegen2.gif";
     console.log("Ingresa a saludar");
 }
