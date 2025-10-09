e3c0843cd6186f940bfb1a8be56e4e342a2c8a2e
RubenNacimba <mundoinformaticoz.com@gmail.com>
2025-10-08 22:11:21 -0500

    Ejercicio impar terminado

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,44 @@
+mostrarNumeros = function () {
+    console.log("antes del for");
+    for (let i = 0; i < 4; i++) {
+        console.log(i);
+    }
+    console.log("despues del for");
+}
+mostrarNumeros2 = function () {
+    console.log("antes del for");
+    for (let indice = 1; indice <= 5; indice++) {
+        console.log(indice);
+    }
+    console.log("despues del for");
+
+}
+mostrarPares = function () {
+    console.log("antes del for");
+    for (let x = 2; x <= 10; x += 2) {
+        console.log(x);
+    }
+    console.log("despues del for");
+}
+mostrarInverso = function () {
+    console.log("antes del for");
+    for (let i = 10; i > 0; i--) {
+        console.log(i);
+    }
+    console.log("despues del for");
+}
+hackearNasaPelis = function () {
+    //hackeando nasa 0%... 100%
+    for (let porcentaje = 0; porcentaje <= 100; porcentaje += 10) {
+        console.log("hackeando nasa" + porcentaje + "%");
+    }
+    console.log("La nasa ha sido hackeada XD");
+
+}
+mostratImpares = function () {
+    console.log("antes del for");
+    for (let i = 1; i <= 21; i += 2) {
+        console.log(i);
+    }
+    console.log("despues del for");
+}
