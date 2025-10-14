dfd01bfc165bd6e74b1ddeb81d24fcf0c139dee0
RubenNacimba <mundoinformaticoz.com@gmail.com>
2025-10-14 10:07:37 -0500

    Reto 44 terminado evidencia

--- objetosV2/objetos.js
+++ objetosV2/objetos.js
@@ -34,3 +34,61 @@ crearProducto= function(){
         console.log("Ambos tienen el mismo stock");
     }
 }
+modificarAtributos= function(){
+    let cuenta ={
+        numero: "5323423423",
+        saldo:0.0
+    }
+    cuenta.saldo=100;
+    cuenta.saldo+=10;
+    console.log(cuenta.saldo);
+}
+crearCliente=function(){
+    let cliente ={
+        cedula: "1827272",
+        nombre: "Juan",
+    }
+    let cliente1={};
+    cliente1.nombre="Romeo";
+    cliente1.apellido="Salcedo";
+    cliente1.cedula="17263618129";
+}
+
+probarIncrementoSaldo=function(){
+    let cta={numero:"23424", saldo:34.0}
+    incrementarSaldo(cta,100);
+    console.log(cta.saldo);
+}
+
+probarDeterminarMayor=function(){
+    let per1={nombre:"Daniel", edad:45};
+    let per2={nombre:"Luisa", edad:48};
+    let mayor;
+    mayor=determinarMayor(per1,per2);
+    if(mayor!=null){
+        console.log("El mayor es:"+mayor.nombre);
+    }
+
+}
+
+
+
+incrementarSaldo=function(cuenta,monto){
+    cuenta.saldo+=monto;
+}
+
+determinarMayor= function(persona1,persona2){
+    if(persona1.edad>persona2.edad){
+        return persona1;
+    } else if(persona2.edad>persona1.edad){
+        return persona2;
+    }else{
+        return null;
+    }
+
+}
+
+
+
+
+
