1d50511a509d7c7ceaf640edbd784e32cc346d79
RubenNacimba <mundoinformaticoz.com@gmail.com>
2025-10-14 23:24:30 -0500

    Ejercicio 47 terminado

--- roles/roles.js
+++ roles/roles.js
@@ -1,4 +1,4 @@
-let resumenRoles=[]; //Arreglo para almacenar los roles calculados
+let esNuevo=false;
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
     {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
@@ -25,13 +25,23 @@ mostrarEmpleados = function() {
 
 }
 
-
-
 mostrarOpcionEmpleado=function(){
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
     mostrarEmpleados();  // se llama aqui para que se muestre al cargar
+     deshabilitarCamposEmpleado();
+
+    
+}
+deshabilitarCamposEmpleado=function(){
+    deshabilitarComponente("txtCedula");
+    deshabilitarComponente("txtNombre");
+    deshabilitarComponente("txtApellido");
+    deshabilitarComponente("txtSueldo");
+    deshabilitarComponente("btnGuardar");
+
+
 }
 mostrarOpcionRol=function(){
     mostrarComponente("divRol");
@@ -43,3 +53,99 @@ mostrarOpcionResumen=function(){
     ocultarComponente("divRol");
     ocultarComponente("divEmpleado");
 }
+
+ejecutarNuevo=function(){
+    habilitarComponente("txtCedula");
+    habilitarComponente("txtNombre");
+    habilitarComponente("txtApellido");
+    habilitarComponente("txtSueldo");
+    habilitarComponente("btnGuardar");
+    esNuevo=true;
+}
+//Función que busca a un empleado en el arreglo 'empleados' segun su cedula
+buscarEmpleado = function(cedula){
+    for(let i =0; i< empleados.length;i++ ){
+        //Verificamos si la cedula del empleado actual coincide con la cedula buscada
+        if(empleados[i].cedula==cedula){
+            //Si coincide, retornamos el objeto empleado encontrado
+            return empleados[i];
+        }
+    }
+    // Si no encuentra ningun empleado con esa cédula, retornamos null
+    return null;
+}
+//Función que agrega un nuevo empleado al arreglo 'empleados'
+agregarEmpleado=function(empleado){
+    //Verificar si ya existe un empleado con la misca cedula
+    let existente = buscarEmpleado(empleado.cedula);
+    //Si no existe, lo agregamos al arreglo
+    if(existente==null){
+        empleados.push(empleado);
+        return true; // Se agrego correctamente
+    } else {
+        return false; // Ya existe un empleado con esa cédula
+    }
+}
+// Función que guarda un nuevo empelado si esNuevo es true
+guardar= function(){
+    //Recuperar valores desde la caja de texto
+    let cedula = recuperarTexto("txtCedula");
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+    let sueldo = recuperarFloat("txtSueldo");
+
+    let valido= true;
+    //Validación de cedula: 10 digitos exactos
+    if(cedula.length !==10 || !/^\d{10}$/.test(cedula)){
+        mostrarTexto("lblErrorCedula", "Cédula invalida");
+        valido=false;
+    }else {
+        mostrarTexto("lblErrorCedula", "");
+    }
+    //Validación de nombre: más de 3 letras
+    if(nombre.length <=3 || !/^[a-zA-ZÁÉÍÓÚáéíóúÑñ\s]+$/.test(nombre)){
+        mostrarTexto("lblErrorNombre", "Nombre inválido");
+        valido= false;
+    }else{
+        mostrarTexto("lblErrorNombre", "");
+    }
+    //Validación de apellido: más de 3 letras
+    if(apellido.length<=3 || !/^[a-zA-Z]+$/.test(apellido)){
+        mostrarTexto("lblErrorApellido", "Apellido invalido ");
+        valido= false;
+    }else {
+        mostrarTexto("lblErrorApellido", "");
+    }
+    // Validacion de suelo: número entre 400 y 500
+    if(isNaN(sueldo) || sueldo<400 || sueldo>5000){
+        mostrarTexto("lblErrorSueldo", "Sueldo inválido");
+        valido=false;
+    }else{
+        mostrarTexto("lblErrorSueldo", "");
+    }
+    // Si alguna validación falla, no continua
+    if(!valido) return;
+    //Si es un nuevo registro 
+    if(esNuevo){
+        // Crear objeto empleado
+        let nuevoEmpleado={
+            cedula: cedula,
+            nombre: nombre,
+            apellido: apellido,
+            sueldo: sueldo
+        };
+        //Intentar agregar al arreglo 
+        let agregado = agregarEmpleado(nuevoEmpleado);
+        //mostrar mensaje segun resultado
+        if(agregado){
+            mostrarTexto("lblErrorBusquedaEmpleado","EMPLEADO GUARDADO CORRECTAMENTE");
+            mostrarEmpleados(); //Actualizar la tabla
+        } else{
+            mostrarTexto("lblErrorBusquedaEmpleado", "YA EXISTE UN EMPLEADO CON LA CÉDULA"+cedula);
+        }
+    }
+    //Deshabilitar campos después de guardar
+    deshabilitarCamposEmpleado();
+}
+
+
